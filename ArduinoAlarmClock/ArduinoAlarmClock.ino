aa461d4c3b871e91597d755d33ba4e79bddf2722
David Koudela <dkoudela@volny.cz>
2018-06-23 18:19:49 +0200

    Added 1602 LCD support.

--- ArduinoAlarmClock/ArduinoAlarmClock.ino
+++ ArduinoAlarmClock/ArduinoAlarmClock.ino
@@ -3,9 +3,13 @@
 // připojení knihoven
 #include <Wire.h>
 #include "RTClib.h"
+#include <LiquidCrystal.h>
 
 // vytvoření instance DS1307 z knihovny RTClib
 RTC_DS1307 DS1307;
+
+// initialize the library with the numbers of the interface pins 
+LiquidCrystal lcd(12,11,10,9,8,7,6,5,4,3,2);       
 
 // vytvoření pole seznamDni s názvy jednotlivých dní
 char seznamDni[7][8] = {"nedele", "pondeli", "utery", "streda", "ctvrtek", "patek", "sobota"};
@@ -25,7 +29,10 @@ void setup () {
   // pro nastavení času v obvodu reálného času použijte jednorázově(!)
   // následující příkaz v pořadí rok, měsíc, den, hodina, minuta, vteřina
   // příklad: 26.4.2016 9:10:11
-  DS1307.adjust(DateTime(2018, 6, 22, 14, 13, 00));
+  //DS1307.adjust(DateTime(2018, 6, 23, 17, 52, 00));
+
+  lcd.begin(16,2);      // set up the LCD's number of columns and rows: 
+  lcd.setCursor(0,0);   // set the cursor to column 0, line 0 
 }
 
 void loop () {
@@ -47,6 +54,28 @@ void loop () {
   Serial.print('.');
   Serial.print(datumCas.year());
   Serial.println();
+
+  displayDateTime(datumCas);
+  
   // pauza mezi výpisy
   delay(1000);
 }
+
+void displayDateTime(DateTime dateTime) {
+  lcd.clear();
+  lcd.print(seznamDni[dateTime.dayOfTheWeek()]);
+  lcd.print(" ");
+  lcd.print(dateTime.day());
+  lcd.print('.');
+  lcd.print(dateTime.month());
+  lcd.print('.');
+  lcd.print(dateTime.year());
+
+  lcd.setCursor(4,2);   // set the cursor to column 0, line 2 
+  lcd.print(dateTime.hour());
+  lcd.print(':');
+  lcd.print(dateTime.minute());
+  lcd.print(':');
+  lcd.print(dateTime.second());
+}
+
